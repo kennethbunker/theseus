c786a0f0cd8c64d67b340fd7ff2aac6d8319d42f
Kenneth Bunker <kabunker@gmail.com>
2025-06-27 11:32:42 +0200

    ss

---
+++ chapter 5.R
@@ -0,0 +1,13 @@
+###################################################
+# R SCRIPT
+# 
+# author          : Kenneth Bunker
+# permalink       : https://kennethbunker.github.io/theseus
+# corresponding   : kenneth.bunker@uss.cl
+# book            : Chile’s Transformation Amdist Crisis: The Theseus Paradox
+# chapter         : Chapter 5: The paradox of progress
+# chapter doi     : 
+# data doi        : https://doi.org/10.7910/DVN/UNM2S7
+# script          :   
+#
+###################################################
